0d05ebf9b4477465031cfe36e2b4d74868822a69
Murilo Hallgren <murilohallgren@gmail.com>
2022-07-18 23:38:55 -0300

    tela de login e botoes

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { NativeBaseProvider , StatusBar } from 'native-base';
+// import { SignIn } from './src/telas/SignIn';
+import { Home } from './src/telas/Home';
+import { THEME } from './src/styles/theme';
+import { useFonts, Roboto_400Regular, Roboto_700Bold  } from '@expo-google-fonts/roboto';
+import { Loading } from './src/comp/Loading';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      {fontsLoaded ? <Home /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
+
+
