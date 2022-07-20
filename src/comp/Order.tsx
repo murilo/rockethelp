cbe2d63585f971e613602eb8303e0aae86302e45
Murilo Hallgren <murilohallgren@gmail.com>
2022-07-19 23:26:40 -0300

    detalhes

--- src/comp/Order.tsx
+++ src/comp/Order.tsx
@@ -18,14 +18,8 @@ export function Order({ data, ...rest }: Props) {
     const statusColor = data.status === 'open' ? colors.secondary[700] : colors.green[300];
     return (
         <Pressable {...rest}>
-            <HStack
-                bg="gray.600"
-                mb={4}
-                alignItems="center"
-                justifyContent="space-between"
-                rounded="sm"
-                overflow="hidden"
-            >
+            <HStack bg="gray.600" mb={4} alignItems="center" justifyContent="space-between"
+                rounded="sm" overflow="hidden" >
                 <Box h="full" w={2} bg={statusColor} />
                 <VStack flex={1} my={5} ml={5} >
                     <Text color="white" fontSize="md">
--- src/telas/SignIn.tsx
+++ src/telas/SignIn.tsx
@@ -1,4 +1,4 @@
-import { useState }  from 'react';
+import { useState } from 'react';
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import Logo from '../assets/logo_primary.svg';
 import { Input } from '../comp/Input';
@@ -15,31 +15,31 @@ export function SignIn() {
     function handleSignIn() {
     }
 
-  return (
-      <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
-          <Logo />
-          <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
-              Acesse sua conta
-          </Heading>
+    return (
+        <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+            <Logo />
+            <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+                Acesse sua conta
+            </Heading>
 
-          <Input
-              placeholder="E-mail"
-              mb={4}
-              InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
-              onChangeText={text => setNome(text)}
-          />
-        
-          <Input
-              mb={8}
-              placeholder="Senha"
-              InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
-              secureTextEntry
-              onChangeText={text => setSenha(text)}
+            <Input
+                placeholder="E-mail"
+                mb={4}
+                InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+                onChangeText={text => setNome(text)}
+            />
 
-          />
+            <Input
+                mb={8}
+                placeholder="Senha"
+                InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+                secureTextEntry
+                onChangeText={text => setSenha(text)}
 
-          <Button title="Entrar" w="full" mb={2} onPress={handleSignIn} />
+            />
 
-      </VStack>
-  );
+            <Button title="Entrar" w="full" mb={2} onPress={handleSignIn} />
+
+        </VStack>
+    );
 }
